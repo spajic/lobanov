74838d92e2c12db336fc7eb1eb4246cb14759939
Alexey Vasilyev <a.vasilyev@3commas.io>
2022-02-02 16:04:35 +0300

    Move a couple of files

--- lib/lobanov.rb
+++ lib/lobanov.rb
@@ -1,6 +1,44 @@
-require "lobanov/version"
+# frozen_string_literal: true
 
 module Lobanov
-  class Error < StandardError; end
-  # Your code goes here...
+  class LobanovError < StandardError; end
+
+  class SchemaMismatchError < LobanovError; end
+
+  class NonroutableRequestError < LobanovError; end
+
+  def self.capture(&block)
+    Spy.on(&block)
+  end
+
+  def self.capture!(&block)
+    Spy.on!(&block)
+  end
+
+  def self.handle_captured_interactions(interactions, overwrite: false)
+    if interactions.size > 1
+      raise LobanovError, 'Lobanov only supports one interaction per spec yet'
+    end
+
+    interaction = interactions.first
+    repo = Repo.new(interaction: interaction)
+    stored_schema = repo.load_schema
+
+    if overwrite || stored_schema.blank?
+      repo.store_schema
+    else
+      new_schema = Generator.new(interaction: interaction).call
+      error = Validator.call(new_schema: new_schema, stored_schema: stored_schema)
+      return unless error
+
+      raise SchemaMismatchError, build_error_message(interaction, error)
+    end
+  end
+
+  def self.build_error_message(interaction, validation_error)
+    interaction_name = "#{interaction.verb} #{interaction.endpoint_path}"
+    "LOBANOV DETECTED SCHEMA MISMATCH!\n\n" \
+      "Interaction '#{interaction_name}' failed! Schema changed:\n" \
+      "#{validation_error}\n\n"
+  end
 end
---
+++ lib/lobanov/spy.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Lobanov
+  class Spy
+    def initialize(&block)
+      @block = block
+      @interactions = []
+    end
+
+    def self.on(&block)
+      spy = new(&block)
+      Thread.current[:lobanov_spy] = spy
+      spy.call
+    ensure
+      Thread.current[:lobanov_spy] = nil
+    end
+
+    def self.on!(&block)
+      spy = new(&block)
+      Thread.current[:lobanov_spy] = spy
+      spy.call(overwrite: true)
+    ensure
+      Thread.current[:lobanov_spy] = nil
+    end
+
+    def self.current
+      Thread.current[:lobanov_spy]
+    end
+
+    def self.enabled?
+      current.present?
+    end
+
+    def call(overwrite: false)
+      @block.call
+      Lobanov.handle_captured_interactions(@interactions, overwrite: overwrite)
+    end
+
+    def add_interaction_by_action_dispatch(request, response)
+      @interactions << Interaction.from_action_dispatch(request, response)
+    end
+  end
+end
