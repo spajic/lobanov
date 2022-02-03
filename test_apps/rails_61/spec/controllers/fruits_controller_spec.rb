b6d5ec47820e234a022178e29c295174a71f3e46
Alexey Vasilyev <bestspajic@gmail.com>
2022-02-03 18:04:20 +0300

    Add specs for both syntax form of Lobanov capture

--- test_apps/rails_61/spec/controllers/fruits_controller_spec.rb
+++ test_apps/rails_61/spec/controllers/fruits_controller_spec.rb
@@ -6,7 +6,17 @@ RSpec.describe FruitsController, type: :controller do
   describe 'index' do
     subject { get :index, params: { }, format: :json }
 
+    # with Lobanov.capture block syntax
     it 'returns expected json', :aggregate_failures do
+      Lobanov.capture do
+        subject
+      end
+      expect(response).to have_http_status(:ok)
+      expect(json_body).to eq({fruits: 'will_be_here'})
+    end
+
+    # with :lobanov rspec-tag syntax
+    it 'returns expected json', :aggregate_failures, :lobanov do
       subject
       expect(response).to have_http_status(:ok)
       expect(json_body).to eq({fruits: 'will_be_here'})
