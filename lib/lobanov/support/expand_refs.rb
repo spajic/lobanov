e505041e5e9075c2c312aab409430ed526fb2169
Alexey Vasilyev <bestspajic@gmail.com>
2023-02-15 17:21:15 +0300

    Make exped_refs spec pass with relative ref fixture

--- lib/lobanov/support/expand_refs.rb
+++ lib/lobanov/support/expand_refs.rb
@@ -3,26 +3,28 @@
 module Lobanov
   module Support
     class ExpandRefs
-      def self.call(schema, index_folder)
+      def self.call(schema, current_folder)
+        current_path = Pathname.new(current_folder)
+
         Lobanov::RefVisitor.visit(schema).each do |node|
           path = node[:path]
           value = node[:value]
 
           ref = value['$ref']
+          ref_path = current_path.join(ref)
+          ref_folder = ref_path.parent
 
-          if ref.start_with?('#')
-            raise LobanovError, "Reference to internal schema is not supported yet: #{ref}"
+          ref_schema = Support.read_relative_from_path(ref, current_path)
+          expanded_ref_schema = self.call(ref_schema, ref_folder) # recursion here
+          if path == [] # expanded schema contains only the ref and nothing else
+            schema = expanded_ref_schema
+          elsif path.size == 1
+            schema[path.last] = expanded_ref_schema
           else
-            ref_schema = Support.read_relative_from_path(ref, index_folder)
-            expanded_ref_schema = self.call(ref_schema, index_folder) # recursion here
-            if path == [] # expanded schema contains only the ref and nothing else
-              schema = expanded_ref_schema
-            else
-              schema.dig(*path[0..-2])[path.last] = expanded_ref_schema 
-            end
+            schema.dig(*path[0..-2])[path.last] = expanded_ref_schema 
           end
         end
-
+        
         schema
       end
     end
--- spec/support/expand_refs_spec.rb
+++ spec/support/expand_refs_spec.rb
@@ -4,14 +4,14 @@ require 'spec_helper'
 
 RSpec.describe Lobanov::Support::BundleSchema do 
   let(:subject) do 
-    Lobanov::Support::ExpandRefs.call(schema, index_folder)
+    Lobanov::Support::ExpandRefs.call(schema, current_folder)
   end
 
   describe 'call' do
     context 'with schema without internal #references' do
-      let(:index_folder) { 'spec/fixtures/bundle_schema/examples/verbose' }
-      let(:schema) { YAML.load_file(index_folder + '/index.yaml') } 
-      let(:etalon) { YAML.load_file(index_folder + '/verbose_etalon.yaml') }
+      let(:current_folder) { 'spec/fixtures/bundle_schema/examples/verbose' }
+      let(:schema) { YAML.load_file(current_folder + '/index.yaml') } 
+      let(:etalon) { YAML.load_file(current_folder + '/verbose_etalon.yaml') }
       it 'returns expected etalon result' do 
         expect(subject).to eq(etalon)
       end
