175fb7f02f00848fb4f85d9f2e57dafd28d99f9e
Alexey Vasilyev <bestspajic@gmail.com>
2023-03-04 13:46:03 +0300

    Remove skipped spec

--- spec/ref_to_register_visitor_spec.rb
+++ spec/ref_to_register_visitor_spec.rb
@@ -37,16 +37,4 @@ RSpec.describe Lobanov::RefToRegisterVisitor do
   it 'emits expected nodes' do 
     expect(subject.to_a).to eq(expected_nodes)
   end
-
-  # Make this test work!
-  xit 'does not modify incoming schema' do 
-    incoming_schema = schema.dup
-    described_class.visit(
-      schema: incoming_schema, 
-      registered_components: registered_components, 
-      current_folder: root_folder
-    ).to_a
-    
-    expect(incoming_schema).to eq(schema)
-  end
 end
